629065e54a1599fa137dcc30c3eab65ea76a7f11
Inge Heeringa <inge.heeringa@gmail.com>
2023-02-22 23:27:53 +0100

    Implement toggle complete functionality in MonthlyGoal component

--- src/components/MonthlyGoal/MonthlyGoal.tsx
+++ src/components/MonthlyGoal/MonthlyGoal.tsx
@@ -1,3 +1,5 @@
+import { toggleCompleteActionCreator } from "../../store/features/goalsSlice";
+import { useAppDispatch } from "../../store/hooks";
 import { Goal } from "../../types";
 import MonthlyGoalStyled from "./MonthlyGoalStyled";
 
@@ -5,10 +7,20 @@ interface MonthlyGoalProps {
   goal: Goal;
 }
 const MonthlyGoal = ({ goal }: MonthlyGoalProps): JSX.Element => {
+  const dispatch = useAppDispatch();
+
+  const handleToggleComplete = () => {
+    dispatch(toggleCompleteActionCreator(goal.id));
+  };
+
   return (
     <MonthlyGoalStyled>
-      <input type="checkbox" checked={goal.isCompleted} />
-      <span>{goal.title}</span>
+      <input
+        type="checkbox"
+        checked={goal.isCompleted}
+        onChange={() => handleToggleComplete()}
+      />
+      <span className={goal.isCompleted ? "completed" : ""}>{goal.title}</span>
     </MonthlyGoalStyled>
   );
 };
